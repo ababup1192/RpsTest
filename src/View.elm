9c86f53e5a9bf42f54e6acecd0d75408d3fd15d7
abab <ababup1192@gmail.com>
2017-12-05 18:08:38 +0900

    Inject rps logic

--- src/View.elm
+++ src/View.elm
@@ -5,11 +5,11 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Messages exposing (..)
 import Models exposing (Model, Game(..))
-import Rps exposing (Hand(..))
+import Rps exposing (Hand(..), Result(..), fight)
 
 
 view : Model -> Html Msg
-view { game } =
+view { game, you, enemy } =
     case game of
         Start ->
             input
@@ -20,17 +20,54 @@ view { game } =
                 []
 
         NowPlaying ->
-            input
-                [ type_ "button"
-                , onClick <| SelectHand Rock
-                , value "Rock"
-                ]
-                []
+            div [] hands
 
         Over ->
-            input
-                [ type_ "button"
-                , onClick NextGame
-                , value "次のゲームへ"
-                ]
-                []
+            let
+                ye =
+                    (toString you) ++ " VS " ++ (toString enemy)
+
+                r =
+                    result you enemy
+            in
+                div []
+                    [ h1 [] [ text ye ]
+                    , r
+                    , input
+                        [ type_ "button"
+                        , onClick NextGame
+                        , value "次のゲームへ"
+                        ]
+                        []
+                    ]
+
+
+hands : List (Html Msg)
+hands =
+    [ Rock, Paper, Scissors ]
+        |> List.map
+            (\hand ->
+                input
+                    [ type_ "button"
+                    , onClick <| SelectHand hand
+                    , value <| toString hand
+                    ]
+                    []
+            )
+
+
+result : Hand -> Hand -> Html Msg
+result you enemy =
+    let
+        r =
+            Rps.fight you enemy
+    in
+        case r of
+            Win ->
+                h1 [ style [ ( "color", "red" ) ] ] [ text <| toString r ]
+
+            Lose ->
+                h1 [ style [ ( "color", "blue" ) ] ] [ text <| toString r ]
+
+            Draw ->
+                h1 [ style [ ( "color", "gray" ) ] ] [ text <| toString r ]
